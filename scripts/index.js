2eeb75fa54eb17de50c70b3daa8cafe4318aefab
Elena Zotova <elena.ztv.disk@yandex.ru>
2022-12-07 14:21:26 +0300

    move cards with places from html to js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,67 @@
-//variables
+
 const page = document.querySelector('.page');
 const editButton = page.querySelector('.profile__edit-button');
-const popup = page.querySelector('.popup');
 
+//popup variables
+const popup = page.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.input_type_full-name');
 const jobInput = formElement.querySelector('.input_type_job');
 const closeButton = page.querySelector('.popup__close-button');
 
+//profile variables
 const fullName = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
 
-//functions
+//card variables
+const cardTemplate = document.querySelector('#card-template').content;
+const placesGrid = document.querySelector('.places-grid');
+
+const initialCards = [
+  {
+    name: 'Гора Эльбрус',
+    link: './images/grid-elbrus.jpg',
+    alt: 'Вид на гору Эльбрус.'
+  },
+  {
+    name: 'Домбай',
+    link: './images/grid-dombai.jpg',
+    alt: 'Заснеженные вершины Домбая.'
+  },
+  {
+    name: 'Куршская коса',
+    link: './images/grid-kosa.jpg',
+    alt: 'Берег Куршской косы.'
+  },
+  {
+    name: 'Алтай',
+    link: './images/grid-altai.jpg',
+    alt: 'Ноутбук на фоне лесов Алтая.'
+  },
+  {
+    name: 'Байкал',
+    link: './images/grid-baikal.jpg',
+    alt: 'Лед на озере Байкал.'
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: './images/grid-karachaevsk.jpg',
+    alt: 'Сентинский храм в Карачаево-Черкессии.'
+  }
+];
+
+//add six cards on page load
+initialCards.forEach(function(element) {
+  const cardElement = cardTemplate.querySelector('.places-grid__card').cloneNode(true);
+
+  cardElement.querySelector('.places-grid__photo').src = element.link;
+  cardElement.querySelector('.places-grid__photo').alt = element.alt;
+  cardElement.querySelector('.places-grid__name').textContent = element.name;
+
+  placesGrid.append(cardElement);
+});
+
+//open and close profile
 function openProfile() {
   popup.classList.add('popup_opened');
   nameInput.value = fullName.textContent;
@@ -22,7 +72,7 @@ function closeProfile() {
   popup.classList.remove('popup_opened');
 }
 
-// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+// form submit handler, but it's not yet sent anywhere
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
@@ -31,10 +81,7 @@ function formSubmitHandler (evt) {
   popup.classList.remove('popup_opened');
 }
 
-
-//eventListeners
+//eventListeners for profile
 editButton.addEventListener('click', openProfile);
-
 closeButton.addEventListener('click', closeProfile);
-
 formElement.addEventListener('submit', formSubmitHandler);
