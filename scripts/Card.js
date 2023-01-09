1a6cbcfe921d59993fdc494f74ef756a25065a27
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-01-09 14:21:49 +0400

    fix: console errors

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,7 +1,17 @@
 export default class Card {
   constructor(name, link, alt) {
-    this.name = name;
+    this._name = name;
     this._link = link;
     this._alt = alt;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+    .querySelector('#card-template')
+    .content
+    .cloneNode(true);
+
+  // вернём DOM-элемент карточки
+    return cardElement;
   }
 }
